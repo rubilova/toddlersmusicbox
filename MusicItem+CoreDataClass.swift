bd3a047e4ddcd0546047603a296fb29b881e261a
Elena Rubilova <yelena.rubilova@gmail.com>
2017-03-30 16:01:15 -0400

    first commit

---
+++ MusicItem+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  MusicItem+CoreDataClass.swift
+//  
+//
+//  Created by Yelena Rubilova on 2/19/17.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(MusicItem)
+public class MusicItem: NSManagedObject {
+
+}
